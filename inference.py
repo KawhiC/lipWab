7b987adf49ec36b8fc9b7af14e1155ebfb6b697a
Lei <139673754+KawhiC@users.noreply.github.com>
2024-11-06 19:38:59 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,4 @@
 from os import listdir, path
-import sys
-import streamlit as st
 import numpy as np
 import scipy, cv2, os, sys, argparse, audio
 import json, subprocess, random, string
@@ -10,53 +8,56 @@ import torch, face_detection
 from models import Wav2Lip
 import platform
 
-parser = argparse.ArgumentParser(description='Inference code to lip-sync videos in the wild using Wav2Lip models')
 
-parser.add_argument('--checkpoint_path', type=str, 
-					help='Name of saved checkpoint to load weights from', required=True)
+def parse_args():
+	parser = argparse.ArgumentParser(description='Inference code to lip-sync videos in the wild using Wav2Lip models')
 
-parser.add_argument('--face', type=str, 
-					help='Filepath of video/image that contains faces to use', required=True)
-parser.add_argument('--audio', type=str, 
-					help='Filepath of video/audio file to use as raw audio source', required=True)
-parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.', 
-								default='results/result_voice.mp4')
+	parser.add_argument('--checkpoint_path', type=str, 
+						help='Name of saved checkpoint to load weights from', required=True)
 
-parser.add_argument('--static', type=bool, 
-					help='If True, then use only first video frame for inference', default=False)
-parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)', 
-					default=25., required=False)
+	parser.add_argument('--face', type=str, 
+						help='Filepath of video/image that contains faces to use', required=True)
+	parser.add_argument('--audio', type=str, 
+						help='Filepath of video/audio file to use as raw audio source', required=True)
+	parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.', 
+									default='results/result_voice.mp4')
 
-parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0], 
-					help='Padding (top, bottom, left, right). Please adjust to include chin at least')
+	parser.add_argument('--static', type=bool, 
+						help='If True, then use only first video frame for inference', default=False)
+	parser.add_argument('--fps', type=float, help='Can be specified only if input is a static image (default: 25)', 
+						default=25., required=False)
 
-parser.add_argument('--face_det_batch_size', type=int, 
-					help='Batch size for face detection', default=16)
-parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
+	parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0], 
+						help='Padding (top, bottom, left, right). Please adjust to include chin at least')
 
-parser.add_argument('--resize_factor', default=1, type=int, 
-			help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
+	parser.add_argument('--face_det_batch_size', type=int, 
+						help='Batch size for face detection', default=16)
+	parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
 
-parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1], 
-					help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. ' 
-					'Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width')
+	parser.add_argument('--resize_factor', default=1, type=int, 
+				help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
 
-parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1], 
-					help='Specify a constant bounding box for the face. Use only as a last resort if the face is not detected.'
-					'Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).')
+	parser.add_argument('--crop', nargs='+', type=int, default=[0, -1, 0, -1], 
+						help='Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. ' 
+						'Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width')
 
-parser.add_argument('--rotate', default=False, action='store_true',
-					help='Sometimes videos taken from a phone can be flipped 90deg. If true, will flip video right by 90deg.'
-					'Use if you get a flipped result, despite feeding a normal looking video')
+	parser.add_argument('--box', nargs='+', type=int, default=[-1, -1, -1, -1], 
+						help='Specify a constant bounding box for the face. Use only as a last resort if the face is not detected.'
+						'Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).')
 
-parser.add_argument('--nosmooth', default=False, action='store_true',
-					help='Prevent smoothing face detections over a short temporal window')
+	parser.add_argument('--rotate', default=False, action='store_true',
+						help='Sometimes videos taken from a phone can be flipped 90deg. If true, will flip video right by 90deg.'
+						'Use if you get a flipped result, despite feeding a normal looking video')
 
-args = parser.parse_args()
-args.img_size = 96
+	parser.add_argument('--nosmooth', default=False, action='store_true',
+						help='Prevent smoothing face detections over a short temporal window')
 
-if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
-	args.static = True
+	args = parser.parse_args()
+	args.img_size = 96
+	return args
+
+""" if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
+	args.static = True """
 
 def get_smoothened_boxes(boxes, T):
 	for i in range(len(boxes)):
@@ -67,7 +68,7 @@ def get_smoothened_boxes(boxes, T):
 		boxes[i] = np.mean(window, axis=0)
 	return boxes
 
-def face_detect(images):
+def face_detect(args,images):
 	detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D, 
 											flip_input=False, device=device)
 
@@ -107,14 +108,14 @@ def face_detect(images):
 	del detector
 	return results 
 
-def datagen(frames, mels):
+def datagen(args, frames, mels):
 	img_batch, mel_batch, frame_batch, coords_batch = [], [], [], []
 
 	if args.box[0] == -1:
 		if not args.static:
-			face_det_results = face_detect(frames) # BGR2RGB for CNN face detection
+			face_det_results = face_detect(args,frames) # BGR2RGB for CNN face detection
 		else:
-			face_det_results = face_detect([frames[0]])
+			face_det_results = face_detect(args,[frames[0]])
 	else:
 		print('Using the specified bounding box instead of face detection...')
 		y1, y2, x1, x2 = args.box
@@ -180,7 +181,7 @@ def load_model(path):
 	model = model.to(device)
 	return model.eval()
 
-def main():
+def main(args):
 	if not os.path.isfile(args.face):
 		raise ValueError('--face argument must be a valid path to video/image file')
 
@@ -246,7 +247,7 @@ def main():
 	full_frames = full_frames[:len(mel_chunks)]
 
 	batch_size = args.wav2lip_batch_size
-	gen = datagen(full_frames.copy(), mel_chunks)
+	gen = datagen(args, full_frames.copy(), mel_chunks)
 
 	for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen, 
 											total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
@@ -279,5 +280,7 @@ def main():
 	#subprocess.call(command, shell=platform.system() != 'Windows')
 	subprocess.call(command, shell=True)
 
+
 if __name__ == '__main__':
-	main()
+	args = parse_args()
+	main(args)
