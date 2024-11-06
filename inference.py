998389f40cfaa6422f132b0011c00f3e228eaf53
Lei <139673754+KawhiC@users.noreply.github.com>
2024-11-06 16:24:26 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
 from os import listdir, path
 import sys
-print(sys.path)
+import streamlit as st
+st.successs(sys.path)
 import numpy as np
 import scipy, cv2, os, sys, argparse, audio
 import json, subprocess, random, string
