f916b85315a76d48348780fb1be780f4a1ec9f94
Lei <139673754+KawhiC@users.noreply.github.com>
2024-11-06 19:39:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,7 @@
 import streamlit as st
-import numpy as np
 import os
-import subprocess
-import pkg_resources
-
 import sys
-from inference import main
-installed_packages = pkg_resources.working_set
-installed = [f"{pkg.key}=={pkg.version}" for pkg in installed_packages]
-
+from inference import parse_args, main
 
 # 设置标题
 st.title("嘴唇语音处理")
@@ -29,29 +22,17 @@ if uploaded_audio is not None:
         f.write(uploaded_audio.getbuffer())
     st.success("音频文件已成功上传并保存为 audio.mp3")
 
-# 设置命令和参数
-command = [
-    'python', 'inference.py',
-    '--checkpoint_path', 'wav2lip_gan.pth',
-    '--face', 'video.mp4',
-    '--audio', 'audio.mp3'
-]
+
+
 # 当视频和音频文件都上传后，执行命令
 if uploaded_video is not None and uploaded_audio is not None:
     with st.spinner("正在处理，请稍候..."):
-        r# 模拟命令行参数
+        # 模拟命令行参数
         sys.argv = ['inference.py', '--checkpoint_path', 'wav2lip_gan.pth', '--face', 'test.mp4', '--audio', '3s.mp3']
-
+        args = parse_args()
         # 调用 main 函数，它会像从命令行调用一样解析 sys.argv
-        result = main()
+        main(args)
 
-    # 显示输出和错误信息
-    if result.returncode == 0:
-        st.success("处理完成！")
-    else:
-        st.error("处理失败，请检查输出信息。")
-        st.text(result.stderr)
-    st.success(installed)
     # 检查结果文件是否存在，并展示结果视频
     result_video_path = "results/result_voice.mp4"
     if os.path.exists(result_video_path):
